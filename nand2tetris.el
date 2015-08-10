a476399038dae24379bd8d845ecc0ff5b940c8c3
Diego Berrocal <cestdiego@gmail.com>
2015-08-10 16:11:55 -0500

    using `c-populate-syntax-table' makes syntax easier

--- nand2tetris.el
+++ nand2tetris.el
@@ -24,15 +24,7 @@
 
 (defconst nand2tetris-mode-syntax-table
   (let ((table (make-syntax-table)))
-    ;; ' is a string delimiter
-    (modify-syntax-entry ?' "\"" table)
-    ;; " is a string delimiter too
-    (modify-syntax-entry ?\" "\"" table)
-
-    ;; / is punctuation, but // is a comment starter
-    (modify-syntax-entry ?/ ". 12" table)
-    ;; \n is a comment ender
-    (modify-syntax-entry ?\n ">" table)
+    (c-populate-syntax-table table)
     table))
 
 (defvar nand2tetris-mode-map
