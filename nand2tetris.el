162bcc71ff0388c7f3818c7a5005fcb86a082bd2
Diego Berrocal <cestdiego@gmail.com>
2015-08-13 12:41:49 -0500

    Add an identation function WIP.

--- nand2tetris.el
+++ nand2tetris.el
@@ -683,6 +683,41 @@ Interactively, prompt for symbol."
   '(nand2tetris//snippets-initialize))
 
 
+;;; Indentation
+
+(defun nand2tetris-indent-line ()
+  "Indent current line as WPDL code."
+  (interactive)
+  (beginning-of-line)
+  (if (bobp)
+      (indent-line-to 0)		   ; First line is always non-indented
+    (let ((not-indented t) cur-indent)
+      (if (looking-at "^[ \t]*}") ; If the line we are looking at is the end of a block, then decrease the indentation
+          (progn
+            (save-excursion
+              (forward-line -1)
+              (setq cur-indent (- (current-indentation) default-tab-width)))
+            (if (< cur-indent 0) ; We can't indent past the left margin
+                (setq cur-indent 0)))
+        (save-excursion
+          (while not-indented ; Iterate backwards until we find an indentation hint
+            (forward-line -1)
+            (if (looking-at "^[ \t]*}") ; This hint indicates that we need to indent at the level of the END_ token
+                (progn
+                  (setq cur-indent (current-indentation))
+                  (setq not-indented nil))
+              (if (looking-at "^[ \t]*\\(CHIP\\)") ; This hint indicates that we need to indent an extra level
+                  (progn
+                    (setq cur-indent (+ (current-indentation) default-tab-width)) ; Do the actual indenting
+                    (setq not-indented nil))
+                (if (bobp)
+                    (setq not-indented nil)))))))
+      (if cur-indent
+          (indent-line-to cur-indent)
+        ;; If we didn't see an indentation hint, then allow no indentation
+        (indent-line-to 0)))))
+
+
 ;;; Font-lock and syntax
 (defvar nand2tetris-font-lock-keywords
   ;;Keywords
@@ -710,6 +745,7 @@ Interactively, prompt for symbol."
 
   (set (make-local-variable 'comment-start) "// ")
   (set (make-local-variable 'comment-start-skip) "//+\\s-*")
+  (set (make-local-variable 'indent-line-function) 'nand2tetris-indent-line)
 
   (set (make-local-variable 'font-lock-defaults)
        '(nand2tetris-font-lock-keywords nil nil nil nil)))
