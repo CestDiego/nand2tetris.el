2c65e7570dc19524529a337fac5fd247977fa8da
Diego Berrocal <cestdiego@gmail.com>
2015-08-13 14:51:43 -0500

    Add license for company-nand2tetris

--- company-nand2tetris.el
+++ company-nand2tetris.el
@@ -1,4 +1,39 @@
-;;; Company
+;;; company-nand2tetris.el --- Company backend for nand2tetris major mode
+;;; https://www.coursera.org/course/nand2tetris1
+
+;; Copyright (C) 2015 Diego Berrocal
+
+;; Author: Diego Berrocal <cestdiego@gmail.com>
+;; Created: 10 August 2015
+
+;; Keywords: nand2tetris, hdl, company
+;; Homepage: http://www.github.com/CestDiego/nand2tetris.el/
+;; Version: 0.0.1
+;; Package-Requires: ((company "0.5") (cl-lib "0.5.0"))
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+
+;;; Commentary:
+
+;; Useful functions to make following the Nand2Tetris course easier.
+;; See: https://www.coursera.org/course/nand2tetris1
+
+;;; Code:
 (require 'company)
 (require 'cl-lib)
 
