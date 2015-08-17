26b86c3864957dba3d299f488cda389018f08005
Diego Berrocal <cestdiego@gmail.com>
2015-08-17 14:12:01 -0500

    Nand2Tetris Assembler Assemble!

--- nand2tetris-assembler.el
+++ nand2tetris-assembler.el
@@ -9,7 +9,6 @@
 ;; Keywords: nand2tetris-assembler, hdl
 ;; Homepage: http://www.github.com/CestDiego/nand2tetris-assembler.el/
 ;; Version: 0.0.1
-;; Package-Requires: ((company "0.5") (cl-lib "0.5.0") (yasnippet "0.8.0"))
 
 ;; This file is not part of GNU Emacs.
 
@@ -34,35 +33,90 @@
 ;; See: https://www.coursera.org/course/nand2tetris-assembler1
 
 ;;; Code:
+(require 'nand2tetris)
+
 (defconst nand2tetris-assembler--default-symbols-alist
-  '(("SP" #x0)
-    ("LCL" #x1)
-    ("ARG" #x2)
-    ("THIS" #x3)
-    ("THAT" #x4)
-    ("R0" #x0)
-    ("R1" #x1)
-    ("R2" #x2)
-    ("R3" #x3)
-    ("R4" #x4)
-    ("R5" #x5)
-    ("R6" #x6)
-    ("R7" #x7)
-    ("R8" #x8)
-    ("R9" #x9)
-    ("R10" #xA)
-    ("R11" #xB)
-    ("R12" #xC)
-    ("R13" #xD)
-    ("R14" #xE)
-    ("R15" #xF)
-    ("SCREEN" #x4000)
-    ("KBD" #x6000))
-  "Default alist for the Predefined Symbols according to http://www.nand2tetris.org/chapters/chapter%2006.pdf")
+  '(("SP"     .  #x0)
+    ("LCL"    .  #x1)
+    ("ARG"    .  #x2)
+    ("THIS"   .  #x3)
+    ("THAT"   .  #x4)
+    ("R0"     .  #x0)
+    ("R1"     .  #x1)
+    ("R2"     .  #x2)
+    ("R3"     .  #x3)
+    ("R4"     .  #x4)
+    ("R5"     .  #x5)
+    ("R6"     .  #x6)
+    ("R7"     .  #x7)
+    ("R8"     .  #x8)
+    ("R9"     .  #x9)
+    ("R10"    .  #xA)
+    ("R11"    .  #xB)
+    ("R12"    .  #xC)
+    ("R13"    .  #xD)
+    ("R14"    .  #xE)
+    ("R15"    .  #xF)
+    ("SCREEN" .  #x4000)
+    ("KBD"    .  #x6000))
+  "Default alist of Predefined Symbols for the Hack Assembly.
+According to http://www.nand2tetris.org/chapters/chapter%2006.pdf")
+
+(defconst nand2tetris-assembler--comp-symbols-alist
+  '(("0"    .  "0101010")
+    ("1"    .  "0111111")
+    ("-1"   .  "0111010")
+    ("D"    .  "0001100")
+    ("A"    .  "0110000")
+    ("M"    .  "1110000")
+    ("!D"   .  "0001101")
+    ("!A"   .  "0110001")
+    ("!M"   .  "1110001")
+    ("-D"   .  "0001111")
+    ("-A"   .  "0110011")
+    ("-M"   .  "1110011")
+    ("D+1"  .  "0011111")
+    ("A+1"  .  "0110111")
+    ("M+1"  .  "1110111")
+    ("D-1"  .  "0001110")
+    ("A-1"  .  "0110010")
+    ("M-1"  .  "1110010")
+    ("D+A"  .  "0000010")
+    ("D+M"  .  "1000010")
+    ("D-A"  .  "0010011")
+    ("D-M"  .  "1010011")
+    ("A-D"  .  "0000111")
+    ("M-D"  .  "1000111")
+    ("D&A"  .  "0000000")
+    ("D&M"  .  "1000000")
+    ("D|A"  .  "0010101")
+    ("D|M"  .  "1010101"))
+  "Computation symbols relation with their binary representation.")
+
+(defconst nand2tetris-assembler--dest-symbols-alist
+  '((""    .  "000")
+    ("M"   .  "001")
+    ("D"   .  "010")
+    ("A"   .  "100")
+    ("AD"  .  "110")
+    ("MD"  .  "011")
+    ("AM"  .  "101")
+    ("AMD" .  "111"))
+  "Destination symbols relation with their binary representation.")
+
+(defconst nand2tetris-assembler--jump-symbols-alist
+  '((""    .  "000")
+    ("JGT" .  "001")
+    ("JEQ" .  "010")
+    ("JGE" .  "100")
+    ("JLT" .  "110")
+    ("JNE" .  "011")
+    ("JLE" .  "101")
+    ("JMP" .  "111"))
+  "Jump symbols relation with their binary representation.")
 
 (defun nand2tetris-assembler/read-lines (file)
-  "Return a list of non-double-slash begginging lines of the
-contents of FILE."
+  "Return list of non-double-slash begginging lines in FILE."
   (let ((filter-regexp "\s*//.*$"))
     (with-temp-buffer
       (insert-file-contents file)
@@ -70,51 +124,52 @@ contents of FILE."
 
 (defvar nand2tetris-assembler--A-instruction-regexp
   (rx ?@ (group (* digit)))
-  "Regular Expression for an A instruction")
+  "Regular Expression for an A instruction.")
 
 (defvar nand2tetris-assembler--C-instruction-regexp
-  (rx (group (* (or "M" "D" "A")))
-      (or (seq ?=
-               (group
-                (* (or "M" "A" "D" ?+ ?- ?! ?| ?& "1"))
-                (? (or line-end space "/"))))
-          (seq ";"
-               (group (* word)) (? (or line-end space "/")))))
-  "Regular Expression for an A instruction")
+  (rx
+   ;;Destination regexp
+   (? (seq
+    (group (* (or "M" "D" "A")))
+    ?=))
+   ;; Computation regexp
+   (group
+    (* (or "M" "A" "D" ?+ ?- ?! ?| ?& "1" "0"))
+    (? (or line-end space "/")))
+   ;; Jump regexp
+   (? (seq
+       ";"
+       (group (* word))
+       (? (or line-end space "/")))))
+  "Regular Expression for an A instruction.")
 
 (defun nand2tetris-assembler/parser (instruction)
-  "Encapsulates access to the input code. Reads an assembly language command,
-parses it, and provides convenient access to the command’s
-components (fields and symbols). In addition, removes all white
-space and comments"
+  "Provides convenient access to the command’s components (fields and symbols).
+In addition, removes all white space and comments in INSTRUCTION."
   (let ((A-regexp nand2tetris-assembler--A-instruction-regexp)
         (C-regexp nand2tetris-assembler--C-instruction-regexp))
     (cond ((string-match A-regexp instruction)
            `("A" . ,(match-string 1 instruction)))
           ((string-match C-regexp instruction)
-             `("C" . (("dest" . ,(match-string 1 instruction))
-                      ("comp" . ,(match-string 2 instruction))
-                      ("jump" . ,(match-string 3 instruction))))
-           )
-      )
-    )
-  )
+           `("C" . (("dest" . ,(match-string 1 instruction))
+                    ("comp" . ,(match-string 2 instruction))
+                    ("jump" . ,(match-string 3 instruction))))))))
 
 (defun nand2tetris-assembler/format-A-instruction (decimal-number)
-  "Convert DECIMAL-NUMBER to a 15-long string binary
-representation with 0-padding if the binary representaton is not
+  "Convert DECIMAL-NUMBER to a 16-long string binary representation.
+Resulting string has 0-padding if the binary representaton is not
 big enough."
-  (let* ((integer (string-to-int decimal-number))
+  (let* ((integer (string-to-number decimal-number))
          (binary-string (nand2tetris-assembler/int-to-binary-string integer)))
-    (if (<= (length binary-string) 15)
-        (while (not (equal 15 (length binary-string)))
+    (if (<= (length binary-string) 16)
+        (while (not (equal 16 (length binary-string)))
           (setq binary-string (concat "0" binary-string)))
       (error "The A instruction gave a binary representation bigger than 2e15"))
     binary-string))
 
 (defun nand2tetris-assembler/int-to-binary-string (integer)
-  "Convert INTEGER into it's binary representation in string
-format, borrowed from http://stackoverflow.com/a/20577329."
+  "Convert INTEGER into it's binary representation.
+Borrowed from http://stackoverflow.com/a/20577329"
   (let ((res ""))
     (while (not (= integer 0))
       (setq res (concat (if (= 1 (logand integer 1)) "1" "0") res))
@@ -123,31 +178,52 @@ format, borrowed from http://stackoverflow.com/a/20577329."
         (setq res "0"))
     res))
 
+(defun nand2tetris-assembler/format-C-instruction (data-alist)
+  "Return a valid C-Instruction from the fields in DATA-ALIST.
+If one field retrieved by DATA-ALIST isn't matched, error is raised."
+  (let* ((prefix-bits "111")
+         (dest-symbol (or (cdr (assoc "dest" data-alist)) ""))
+         (jump-symbol (or (cdr (assoc "jump" data-alist)) ""))
+         (comp-symbol (or (cdr (assoc "comp" data-alist)) ""))
+         (dest-bits (cdr (assoc dest-symbol nand2tetris-assembler--dest-symbols-alist)))
+         (comp-bits (cdr (assoc comp-symbol nand2tetris-assembler--comp-symbols-alist)))
+         (jump-bits (cdr (assoc jump-symbol nand2tetris-assembler--jump-symbols-alist))))
+    (unless dest-bits
+      (error (concat "The Destination Symbol does not match to the predefined symbols: " dest-symbol)))
+    (unless comp-bits
+      (error (concat "The Computation Symbol does not match to the predefined symbols: " comp-symbol)))
+    (unless jump-bits
+      (error (concat "The Jump Symbol does not match to the predefined symbols: " jump-symbol)))
+
+    (concat prefix-bits comp-bits dest-bits jump-bits)
+    ))
+
 (defun nand2tetris-assembler/process-line (instruction)
+  "Assume INSTRUCTION is valid and output it's binary representation.
+Instruction correctness given by
+http://www.nand2tetris.org/chapters/chapter%2006.pdf Page 106.
+Also the parsed lines do not start with `//' so we can assume it
+will only have a instruction and maybe inline comments that are
+going to be ignored by the regular expressions."
   (let* ((parsed-instruction (nand2tetris-assembler/parser instruction))
          (type (car parsed-instruction))
          (parsed-data (cdr parsed-instruction)))
-    (cond ((equal type "A" )
-           (message
-            (concat
-            "A Instruction: "
-            (nand2tetris-assembler/format-A-instruction parsed-data))))
-          ((equal type "C") (print parsed-data)
-           (message
-            (concat
-            "C Instruction: "
-            (nand2tetris-assembler/format-C-instruction parsed-data))))
-          )))
+    (cond
+     ((equal type "A" )
+      (message
+       (concat "A Instruction: "
+               (nand2tetris-assembler/format-A-instruction parsed-data))))
+     ((equal type "C") (print parsed-data)
+      (message
+       (concat "C Instruction: "
+               (nand2tetris-assembler/format-C-instruction parsed-data)))))))
 
 (defun nand2tetris-assembler/init ()
-  "Init function for the Hack Assembly mode"
+  "Init function for the Hack Assembly mode."
   (interactive)
   (let* ((current-file (buffer-file-name))
          (read-lines   (nand2tetris-assembler/read-lines current-file)))
-    (mapcar 'nand2tetris-assembler/process-line read-lines)
-
-    ))
-
+    (mapcar 'nand2tetris-assembler/process-line read-lines)))
 
 
 ;;; Bindings
@@ -167,7 +243,6 @@ format, borrowed from http://stackoverflow.com/a/20577329."
     (modify-syntax-entry ?* ". 23" table)
     (modify-syntax-entry ?\n "> b" table)
     table))
-
 
 
 ;;; Major Mode
@@ -190,3 +265,4 @@ format, borrowed from http://stackoverflow.com/a/20577329."
                . nand2tetris-assembler-mode))
 
 (provide 'nand2tetris-assembler)
+;;; nand2tetris-assembler.el ends here
